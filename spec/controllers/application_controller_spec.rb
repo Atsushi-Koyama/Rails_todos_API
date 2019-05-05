b1d578cddd9f53f10199142442e206ef544615fa
Atsushi-Koyama <two51ct13th@yahoo.co.jp>
2019-05-05 11:19:58 +0900

    editauthorize

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,23 +4,23 @@ require "rails_helper"
 RSpec.describe ApplicationController, type: :controller do
   # create test user
   let!(:user) { create(:user) }
-  # set headers fo authorization
-  let(:headers) { { 'Authorization' => token_generator(user.id)} }
+   # set headers for authorization
+  let(:headers) { { 'Authorization' => token_generator(user.id) } }
   let(:invalid_headers) { { 'Authorization' => nil } }
 
   describe "#authorize_request" do
     context "when auth token is passed" do
       before { allow(request).to receive(:headers).and_return(headers) }
 
-      # private method authoriza_request return current user
+      # private method authorize_request returns current user
       it "sets the current user" do
-        expect(subject.instance_eval { authoriza_request }).to eq(user)
+        expect(subject.instance_eval { authorize_request }).to eq(user)
       end
     end
 
     context "when auth token is not passed" do
       before do
-        allow(request).to recieve(:headers).and_return(invalid_headers)
+        allow(request).to receive(:headers).and_return(invalid_headers)
       end
 
       it "raises MissingToken error" do
